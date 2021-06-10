401212b84fee4628c07c144c3e50e3ea81c534da
Kotar <violen332@gmail.com>
2021-06-10 17:37:11 +0300

    add type descriptor
    descript dependency

---
+++ IocContainer.Core/Container/TypeDescriptor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IocContainer.Core.Container
+{
+    public class TypeDescriptor
+    {
+        public TypeDescriptor(object implementation)
+        {
+            ServiceType = implementation.GetType();
+            Implementation = implementation;
+        }
+
+        public TypeDescriptor(Type serviceType)
+        {
+            ServiceType = serviceType;
+        }
+
+        public TypeDescriptor(Type serviceType, Type implementationType)
+        {
+            ServiceType = serviceType;
+            ImplementationType = implementationType;
+        }
+
+        public Type ServiceType { get; set; }
+        public Type ImplementationType { get; set; }
+        public object Implementation { get; internal set; }
+    }
+}
